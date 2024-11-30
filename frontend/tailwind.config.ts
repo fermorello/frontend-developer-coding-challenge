23424d07198d1b026c027150dc56f59e29f48b53
fermorello <ferminmorello@gmail.com>
2024-11-30 16:02:26 -0300

    Dependencies Installations

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,18 +1,98 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        gray: {
+          '0': '#FFFFFF',
+          '200': '#E5E5E5',
+          '500': '#5C5C5C',
+          '1000': '#000000',
+        },
+        pink: {
+          '50': 'rgba(255, 0, 174, 0.5)',
+          '100': '#E7C0DB',
+          '200': '#C698B8',
+          '600': '#FF00AE',
+        },
+        violet: {
+          '50': '#E2DCE7',
+          '100': '#E7C0DB',
+          '600': '#6727A6',
+          '900': '#3C1661',
+        },
+        red: {
+          '600': '#D23F63',
+        },
+        green: {
+          '600': '#67C076',
+        },
+        gradient: {
+          linear: 'linear-gradient(#6727A6, #3C1661)',
+        },
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))',
+        },
+      },
+      fontSize: {
+        h1: '3rem',
+        h2: '2.25rem',
+        h3: '1.875rem',
+      },
+      backgroundImage: {
+        image: "url('/images/desktop-bg.png')",
+        gradient: 'linear-gradient(to right, #6727A6, #3C1661)',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
