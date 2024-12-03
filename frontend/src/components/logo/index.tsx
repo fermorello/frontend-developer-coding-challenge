9b617ae335a09253008abcf018a179b4e0f45fea
fermorello <ferminmorello@gmail.com>
2024-12-03 00:05:43 -0300

    New logo styles

--- frontend/src/components/logo/index.tsx
+++ frontend/src/components/logo/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import MyLogo from '../../../public/images/Frame.svg';
 
 const Logo = () => (
-  <div className="w-[24px] h-[24px] rounded-lg p-1 border-2 border-pink-600 bg-gradient-to-r from-[#FF00AE] to-[#FF97DE]">
+  <div className="h-[24px] w-[24px] flex items-center justify-center p-1 rounded-lg border border-pink-600 animate-fadeInUpShort shadow-md">
     <Image priority src={MyLogo} alt="Logo" />
   </div>
 );
