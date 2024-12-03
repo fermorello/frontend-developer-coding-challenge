de34984be7268c252c0a984895998c6ddb42f3cd
fermorello <ferminmorello@gmail.com>
2024-12-03 00:07:56 -0300

    Translate laceholder

--- frontend/src/components/searchbar/index.tsx
+++ frontend/src/components/searchbar/index.tsx
@@ -68,7 +68,7 @@ export default function GameSearchComponent() {
       >
         <div className="relative">
           <CommandInput
-            placeholder="Buscar juegos..."
+            placeholder="Search games..."
             className="placeholder:text-pink-200"
             value={searchValue}
             onValueChange={setSearchValue}
