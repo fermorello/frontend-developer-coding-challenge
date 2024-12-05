3dfffea856bc470fba942c01753ee4eb1b9c5b36
fermorello <ferminmorello@gmail.com>
2024-12-05 17:29:20 -0300

    Responsive Carousel fix

--- frontend/src/components/mobile-media-carousel/index.tsx
+++ frontend/src/components/mobile-media-carousel/index.tsx
@@ -14,22 +14,25 @@ interface MediaCarouselProps {
 
 function MediaCarousel({ game }: MediaCarouselProps) {
   return (
-    <Carousel className="w-full">
-      <CarouselContent className="w-full space-x-2 px-8">
-        {game?.screenshots
-          .map((screenshot: { id: number; url: string }) => (
-            <Image
-              key={screenshot.id}
-              src={`https:${screenshot.url.replace('t_thumb', 't_thumb')}`}
-              alt={game.name}
-              width={132.8}
-              height={132.8}
-              className="rounded-lg w-[82.5px] h-[110px] md:w-[132.8px] md:h-[132.8px]"
-            />
-          ))}
+    <Carousel className="relative w-full flex justify-start">
+      <CarouselContent className="w-full space-x-2">
+        {game?.screenshots.map((screenshot: { id: number; url: string }) => (
+          <Image
+            key={screenshot.id}
+            src={`https:${screenshot.url.replace('t_thumb', 't_thumb')}`}
+            alt={game.name}
+            width={132.8}
+            height={132.8}
+            className="rounded-lg w-[82.5px] h-[110px] md:w-[132.8px] md:h-[132.8px]"
+          />
+        ))}
       </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
+      <div className="absolute top-1/2 left-[-10px] flex items-center justify-center">
+        <CarouselPrevious className="relative left-0 translate-x-0 hover:translate-x-0 hover:bg-primary/90" />
+      </div>
+      <div className="absolute top-1/2 right-[-10px] flex items-center justify-center">
+        <CarouselNext className="relative right-0 translate-x-0 hover:translate-x-0 hover:bg-primary/90" />
+      </div>
     </Carousel>
   );
 }
